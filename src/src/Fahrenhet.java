e0a2362d9467e4328421778aaae9e2351a8ab522
Daniel Burnes <danielburnes@centurylink.net>
2018-10-28 18:57:35 -0500

    project 2

---
+++ src/src/Fahrenhet.java
@@ -0,0 +1,21 @@
+import static org.graalvm.compiler.debug.TTY.printf;
+
+public class Fahrenhet {
+    //*Input temperature in degree celcius,
+    convert to degree fahrenheit*/
+    #include<stido,h>
+    main()
+    {
+        float temp;
+        printf("input temperature in degree Celcius") ;
+        scanf("%f", &temp);
+        printf("equivalent temperature in degree Fahrenheit is %f", temp*1.8+32);
+
+        float temp;
+        printf("input temperature in degree Fahrenheit");
+        scanf("%c", &temp);
+        printf("equivalent temperaturein degree Celcius is %c, temp*1.8-32);
+
+    }
+    }
+}
