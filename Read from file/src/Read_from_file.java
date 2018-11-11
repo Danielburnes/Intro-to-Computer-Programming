b9e96ad907e4835cbe477c91374f542403e71da7
Daniel Burnes <danielburnes@centurylink.net>
2018-11-11 17:21:02 -0600

    here is my read from file

---
+++ Read from file/src/Read_from_file.java
@@ -0,0 +1,61 @@
+
+public class Read_from_file {
+    public static void main(string[] args) throws Exception {
+        // create a file instance
+        java.io.File file = new java.io.File("Four score and seven Years ago")
+
+
+        // create a scanner for the file
+        scanner input = new Scanner(file);
+
+        //read data from a file
+        while (input.hasNext()) {
+            string Name = input.next(four);
+            string mi = input.next(score)
+            string Name = input.next(and);
+            string Name = input.next(seven);
+            string name = inout.next(years);
+            string name = input.next(ago);
+            system.out.println(
+                    name + "four" + Mi + "score" + name + "and" + name + "seven" + name + "years" + name + "ago");
+
+            int[][] multi = new int[20][45]
+            muti[0] = new int[f];
+            multi[1] = new int[o];
+            multi[2] = new int[u];
+            multi[3] = new int[r];
+            multi[4] = new int[ @];
+            multi[5] = new int[s];
+            multi[6] = new int[c];
+            multi[7] = new int[o];
+            multi[8] = new int[r];
+            multi[9] = new int[e];
+            multi[10] = new int[ @]
+            multi[11] = new int[a];
+            multi[12] = new int[n];
+            multi[13] = new int[d];
+            multi[14] = new int[ @];
+            multi[15] = new int[s];
+            multi[16] = new int[c];
+            multi[17] = new int[o];
+            multi[18] = new int[r];
+            multi[19] = new int[e];
+            multi[20] = new int[ @];
+            multi[21] = new int[y];
+            multi[22] = new int[e];
+            multi[23] = new int[a];
+            multi[24] = new int[r];
+            multi[25] = new int[s];
+            multi[26] = new int[ @];
+            multi[27] = new int[a];
+            multi[28] = new int[g];
+            multi[29] = new int[o];
+            multi[30] = new int[ @];
+
+        }
+
+        //close the file
+        input.close();
+    }
+    }
+}
