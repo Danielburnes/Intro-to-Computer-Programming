488496d6c7d39b0a4df681a36a2edd1220655b19
Daniel Burnes <danielburnes@centurylink.net>
2018-11-21 16:29:02 -0600

    this is milestone 3

---
+++ manager/src/manager_class.java
@@ -0,0 +1,18 @@
+My name is Daniel Burnes and all work is of my own doing
+
+
+import java.util.arraylist
+
+public class manager_class {
+    string manager_class
+    arraylist <manager>managerarray = new arraylist<>(8);
+
+    public Nflmanager
+        super();
+        system.out.println("nfl player name");
+        system.out.println("nfl player position");
+        system.out.println("nfl player height");
+        system.out.println("nfl player weight");
+        system.out.println("nfl player age");
+
+}
